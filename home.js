72b020cc1a265e2c266aabc4560ba6718ca276a8
deepaliswarnkar <deepaliswarnkar14@gmail.com>
2023-04-29 20:03:19 +0530

    first commit

---
+++ home.js
@@ -0,0 +1,12 @@
+function loadCoupon(){
+    debugger;
+    document.getElementById('coupon').style.visibility = 'visible';
+    // document.getElementById('content').style.opacity = '0.7'
+
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility = 'hidden';
+    // document.getElementById('main').style.opacity = '1'
+    
+}
